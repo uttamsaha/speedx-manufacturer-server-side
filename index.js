7fe0d8532952a6c08a7df3d98f412bd0d391355e
uttamsaha <uttamsaha982@gmail.com>
2022-05-28 10:39:44 +0600

    jwt token added

--- index.js
+++ index.js
@@ -11,60 +11,90 @@ app.use(cors());
 app.use(express.json());
 require("dotenv").config();
 
-
 //DB connection
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5waco.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   console.log("DB CONNECTED");
-//   client.close();
-// });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-    try{
-        await client.connect();
-        const toolsCollection = client.db("speedx").collection("tools");
-        const reviewsCollection = client.db("speedx").collection("reviews");
-        
-        //getting all tools
-        app.get('/tool', async(req,res) =>{
-            const tools = await toolsCollection.find().toArray();
-            res.send(tools);
-        });
-
-        //getting all reviews
-        app.get('/review', async(req,res) => {
-            const reviews = await reviewsCollection.find().toArray();
-            res.send(reviews);
-        });
-
-        //posting reviews 
-        app.post('/tool', async(req,res) => {
-            const review = req.body;
-            const result = await reviewsCollection.insertOne(review);
-            res.send({ success: true, result });
-        });
-
-        app.get("/tool/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const tool = await toolsCollection.findOne(query);
-            res.send(tool);
-          });
+//verify jwt token
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
     }
-    finally{
+    req.decoded = decoded;
+    next();
+  });
+}
 
-    }
+async function run() {
+  try {
+    await client.connect();
+    const toolsCollection = client.db("speedx").collection("tools");
+    const reviewsCollection = client.db("speedx").collection("reviews");
+    const userCollection = client.db("speedx").collection("users");
+
+    //getting all tools
+    app.get("/tool", async (req, res) => {
+      const tools = await toolsCollection.find().toArray();
+      res.send(tools);
+    });
+
+    //getting all reviews
+    app.get("/review", async (req, res) => {
+      const reviews = await reviewsCollection.find().toArray();
+      res.send(reviews);
+    });
+
+    //posting reviews
+    app.post("/tool", async (req, res) => {
+      const review = req.body;
+      const result = await reviewsCollection.insertOne(review);
+      res.send({ success: true, result });
+    });
+
+    app.get("/tool/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const tool = await toolsCollection.findOne(query);
+      res.send(tool);
+    });
+
+    //storing user email to database
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.send({ result, token });
+    });
+  } finally {
+  }
 }
 run().catch(console.dir);
-app.get('/',(req,res) => {
-    res.send("SpeedX server is running");
-})
+app.get("/", (req, res) => {
+  res.send("SpeedX server is running");
+});
 
-app.listen(port, (req,res) => {
-    console.log('Listening to port: ',port)
-})
+app.listen(port, (req, res) => {
+  console.log("Listening to port: ", port);
+});
