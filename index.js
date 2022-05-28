4bd8bdacf58ae8587e28d4f7a035d16f3d333578
uttamsaha <uttamsaha982@gmail.com>
2022-05-29 03:44:38 +0600

    users delete api added

--- index.js
+++ index.js
@@ -165,6 +165,14 @@ async function run() {
       res.send(result);
     });
 
+    //delete single user from dashboard
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.get("/admin/:email", async (req, res) => {
       const email = req.params.email;
       const user = await userCollection.findOne({ email: email });
